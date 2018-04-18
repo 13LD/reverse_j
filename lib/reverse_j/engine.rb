2a38a9591ba8c00b5438761c7656ae90b43d488c
Dmytro Lysohor <tompla96@ukr.net>
2018-04-18 09:04:31 +0300

    mount engine

--- lib/reverse_j/engine.rb
+++ lib/reverse_j/engine.rb
@@ -1,4 +1,11 @@
 module ReverseJ
   class Engine < ::Rails::Engine
+    isolate_namespace ReverseJ
+    config.reverse_j = ActiveSupport::OrderedOptions.new
+    initializer 'reverse_j.configuration' do |app|
+      app.routes.append do
+        mount ReverseJ::Engine => "/"
+      end
+    end
   end
 end
